c3335760755f3e462511a11f79be55c603ba4330
Muhammad Sefat <muhammadsefat55@gmail.com>
2024-03-26 10:05:54 +0600

    Create json Data

---
+++ src/Components/Pages/Hero.jsx
@@ -0,0 +1,22 @@
+import book from "../../assets/book.png";
+const Hero = () => {
+  return (
+    <div className="p-5 md:px-10 mx-auto">
+      <div className="hero bg-base-200 rounded-md md:h-[80vh]">
+        <div className="hero-content flex-col lg:flex-row-reverse gap-5">
+          <img src={book} />
+          <div className="md:w-[50%]">
+            <h1 className="text-2xl md:text-5xl font-bold pb-8">
+              Books to freshen up your bookshelf
+            </h1>
+            <button className="btn bg-lime-600 text-white font-semibold">
+              View the List
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,7 +1,18 @@
+import { useLoaderData } from "react-router-dom";
+import Hero from "./Hero";
+import Books from "./Books";
+
 const Home = () => {
+  const books = useLoaderData();
   return (
     <div>
-      <h1>i am home</h1>
+      <Hero></Hero>
+      <div>
+        <h2 className="text-3xl font-bold text-center">Books</h2>
+        {books.map((book) => (
+          <Books key={book.id} book={book}></Books>
+        ))}
+      </div>
     </div>
   );
 };
