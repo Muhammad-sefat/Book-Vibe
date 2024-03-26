53b9db2e446535c9f04a61f51a3fd26cd37564d8
Muhammad Sefat <muhammadsefat55@gmail.com>
2024-03-26 10:34:52 +0600

    Showing all single book

--- src/Components/Pages/Books.jsx
+++ src/Components/Pages/Books.jsx
@@ -1,23 +1,43 @@
+import PropTypes from "prop-types";
+import { CiStar } from "react-icons/ci";
 const Books = ({ book }) => {
+  const { bookName, author, image, rating, tags, category } = book;
   return (
-    <div>
-      <div className="card w-96 bg-base-100 shadow-xl">
+    <div className="p-5 border rounded-md">
+      <div className="card bg-base-100 shadow-xl h-full">
         <figure>
-          <img
-            src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-            alt="Shoes"
-          />
+          <img className="p-5 md:h-[50vh]" src={image} alt="Shoes" />
         </figure>
+        <div className="flex flex-wrap py-2 gap-2 border-t border-dashed dark:border-gray-600">
+          {tags.map((tag) => (
+            <a
+              key={tag.id}
+              rel="noopener noreferrer"
+              href="#"
+              className="mx-auto px-4 rounded-sm hover:underline text-lime-600 font-bold"
+            >
+              {tag}
+            </a>
+          ))}
+        </div>
         <div className="card-body">
-          <h2 className="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
+          <h2 className="card-title">{bookName}</h2>
+          <p className="font-medium">By : {author}</p>
+          <hr />
+          <div className="flex justify-between items-center">
+            <p>{category}</p>
+            <div className="flex items-center gap-2">
+              <p>{rating}</p>
+              <CiStar></CiStar>
+            </div>
           </div>
         </div>
       </div>
     </div>
   );
+};
+Books.propTypes = {
+  book: PropTypes.object.isRequired,
 };
 
 export default Books;
--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -8,10 +8,12 @@ const Home = () => {
     <div>
       <Hero></Hero>
       <div>
-        <h2 className="text-3xl font-bold text-center">Books</h2>
-        {books.map((book) => (
-          <Books key={book.id} book={book}></Books>
-        ))}
+        <h2 className="text-3xl font-bold text-center md:pt-8">Books</h2>
+        <div className="grid md:grid-cols-3 gap-5 py-5 md:px-10">
+          {books.map((book) => (
+            <Books key={book.id} book={book}></Books>
+          ))}
+        </div>
       </div>
     </div>
   );
